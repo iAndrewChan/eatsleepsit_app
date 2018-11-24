181fb29076b6c10e1c59142c3f91ea1b413faac9
Andrew Chan <andrew.chan.2014@my.bristol.ac.uk>
2018-11-24 16:09:20 +0000

    added pages

--- App/app.py
+++ App/app.py
@@ -2,5 +2,21 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return "Hello World!"
+
+@app.route("/eat")
+def eat_page():
+    return "eat World!"
+
+@app.route("/help")
+def help_page():
+    return "help World!"
+
+@app.route("/sleep")
+def sleep_page():
+    return "sleep World!"
+
+@app.route("/contact")
+def contact_page():
+    return "contact page"
